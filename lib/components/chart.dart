1d6aa16fbc3cfacc1b89626842e15ac392a5a29f
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2026-02-17 13:15:10 -0300

    feat: atualiza a lógica de cálculo da porcentagem no gráfico e melhora o formulário de transação para incluir a seleção de data

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -47,7 +47,7 @@ class Chart extends StatelessWidget {
               child: ChartBar(
                 label: tr['day'].toString().toUpperCase(),
                 value: tr['value'] as double,
-                percentage: (tr['value'] as double) / _weekTotalValue,
+                percentage: _weekTotalValue == 0 ? 0 : (tr['value'] as double) / _weekTotalValue,
               ),
             );
           }).toList(),
--- lib/main.dart
+++ lib/main.dart
@@ -57,26 +57,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<Transaction> _transactions = [
-    Transaction(
-      id: Random().nextDouble().toString(),
-      title: 'Conta antiga',
-      value: 400.00,
-      date: DateTime.now().subtract(Duration(days: 33)),
-    ),
-    Transaction(
-      id: Random().nextDouble().toString(),
-      title: 'Novo Tênis de Corrida',
-      value: 310.76,
-      date: DateTime.now().subtract(Duration(days: 3)),
-    ),
-    Transaction(
-      id: Random().nextDouble().toString(),
-      title: 'Conta de Luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
-  ];
+  final List<Transaction> _transactions = [];
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((tr) {
@@ -84,12 +65,12 @@ class _HomePageState extends State<HomePage> {
     }).toList();
   }
 
-  _addTransaction(String title, double value) {
+  _addTransaction(String title, double value, DateTime date) {
     final newTransaction = Transaction(
       id: Random().nextDouble().toString(),
       title: title,
       value: value,
-      date: DateTime.now().subtract(Duration(days: 4)),
+      date: date,
     );
 
     setState(() {
